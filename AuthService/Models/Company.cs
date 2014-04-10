871e3041b4b0e74fc2e6ea4d2f640229d786d67c
Brent Stewart <stewart.brent@gmail.com>
2014-04-10 16:09:34 -0500

    Added address fields to Company

--- AuthService/Models/Company.cs
+++ AuthService/Models/Company.cs
@@ -10,6 +10,11 @@ namespace AuthService.Models
 		public Guid Id { get; set; }
 		public String Name { get; set; }
 		public String Phone {get; set; }
+		public String Address1 { get; set; }
+		public String Address2 { get; set; }
+		public String City { get; set; }
+		public String State { get; set; }
+		public String Zip { get; set; }
 		public virtual ICollection<AppUser> Employees { get; set; }
 
 		public Company()
